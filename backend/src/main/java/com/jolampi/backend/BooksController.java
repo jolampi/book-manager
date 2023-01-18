1c784f9d9d0edc0769a6dd1136513e7b000dbb48
Jori Lampi <jori.lampi@iki.fi>
2023-01-18 13:51:41 +0200

    feat: endpoint to add books

--- backend/src/main/java/com/jolampi/backend/BooksController.java
+++ backend/src/main/java/com/jolampi/backend/BooksController.java
@@ -1,17 +1,34 @@
 package com.jolampi.backend;
 
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BooksController {
 
+  /** In-memory array of books. */
+  private final ArrayList<Book> books = new ArrayList<>();
+  /** Simple id generator to guarantee unique values. */
+  private final AtomicLong idGenerator = new AtomicLong();
+
   @GetMapping("/books")
   public ArrayList<Book> getBooks() {
-    ArrayList<Book> books = new ArrayList<>();
-    books.add(new Book(0, "Game Programming Patterns", "Robert Nystrom", ""));
-    books.add(new Book(1, "Programming Pearls", "Jon Bentley", ""));
-    return books;
+    return this.books;
+  }
+
+  @PostMapping("/books")
+  public void postBook(@RequestBody NewBook newBook) {
+    this.books.add(
+        new Book(
+          this.idGenerator.incrementAndGet(),
+          newBook.title(),
+          newBook.author(),
+          newBook.description()
+        )
+      );
   }
 }
---
+++ backend/src/test/java/com/jolampi/backend/BooksControllerTests.java
@@ -0,0 +1,36 @@
+package com.jolampi.backend;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class BooksControllerTests {
+
+  @Value(value = "${local.server.port}")
+  private int port;
+
+  @Autowired
+  private TestRestTemplate restTemplate;
+
+  private static final String urlTemplate = "http://localhost:%s/books";
+
+  @Test
+  public void greetingShouldReturnDefaultMessage() throws Exception {
+    String url = String.format(urlTemplate, port);
+    NewBook newBook = new NewBook("Game Programming Patterns", "Robert Nystrom", "");
+    this.restTemplate.postForObject(url, newBook, String.class);
+    assertThat(this.restTemplate.getForObject(url, String.class))
+      .contains("Game Programming Patterns");
+  }
+
+  @Test
+  void canAddAndGetBook() {
+    //
+  }
+}

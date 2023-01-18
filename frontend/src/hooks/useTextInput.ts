dde0cbb61bec382ebf5d43060df483beec388350
Jori Lampi <jori.lampi@iki.fi>
2023-01-18 20:40:58 +0200

    feat: add new books from frontend

---
+++ frontend/src/hooks/useTextInput.ts
@@ -0,0 +1,19 @@
+import { ChangeEventHandler, Dispatch, useId, useState } from "react"
+
+export interface TextInput {
+  id: string
+  onChange: ChangeEventHandler<HTMLInputElement>
+  type: "text"
+  value: string
+}
+
+export type UseTextInputHook = [TextInput, Dispatch<string>]
+
+export default function useTextInput(initialState: string): UseTextInputHook {
+  const id = useId()
+  const [value, setValue] = useState(initialState)
+
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => setValue(e.target.value)
+
+  return [{ id, onChange, type: "text", value }, setValue]
+}

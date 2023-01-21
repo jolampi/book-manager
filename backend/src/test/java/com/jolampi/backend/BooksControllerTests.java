b251b6acb83a2df9e610018ba9e098cfe513fbcd
Jori Lampi <jori.lampi@iki.fi>
2023-01-21 05:30:16 +0200

    feat(backend): setup postgres for runtime

--- backend/src/test/java/com/jolampi/backend/BooksControllerTests.java
+++ backend/src/test/java/com/jolampi/backend/BooksControllerTests.java
@@ -10,7 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class BooksControllerTests {
 
